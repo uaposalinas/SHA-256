162d77e82ded1a280eb75a034adc60d85888ec7d
devlabscorporation <121150895+devlabscorporation@users.noreply.github.com>
2024-01-27 02:21:18 -0600

    Add files via upload

---
+++ ID/05012024271240212/com.config.js
@@ -0,0 +1,9 @@
+const ClickButton = document.querySelector('.ClickButton');
+
+ClickButton.addEventListener('click', Activate);
+
+function Activate(){
+
+    document.body.classList.add('Show')
+
+}
